2e63585b033ccf3765b7f3e871b3b981aaca9be0
Andrea <andrea.saitta.as@gmail.com>
2023-07-13 16:37:49 +0200

    dati Andrea

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let members = [];
+let members = [
+  {
+    nome: 'Andrea',
+    cognome: 'Saitta',
+    age: 20,
+    city: 'Bronte',
+    hobby: 'Video Games, Anime',
+    favoriteFood: 'Pizza',
+    favoriteVideoGame: 'AC:Black Flag',
+    favoriteFilm: '',
+    favoriteBook: '',
+    petName: 'Luna'
+  }
+];
