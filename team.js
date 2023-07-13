21685ec84c8893ce9006e79c92c1631652bfe34a
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-07-13 16:40:10 +0200

    dati Desy

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-let members = [];
+let members = [{
+ name: "Desirè",
+ surname:"Passalacqua",
+ age: 25,
+ city: "Catania",
+ hobby: "videogames",
+ favoriteFood: "Pizza",
+ favoriteVideoGame: "The Last of Us part II",
+ favoriteFilm:"Joker",
+ favoriteBook:"La storia infinita",
+ petName: "Trilly"
+}];
