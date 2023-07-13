8107fb3ddc824f0f49e447f03db5802ac3807ba0
Luca Lai <lucaloi1889@gmail.com>
2023-07-13 16:47:00 +0200

    luca

--- team.js
+++ team.js
@@ -1,1 +1,13 @@
-let members = [];
+let members = [{name: "Daniele",
+surname: "Lupo",
+age: 23,
+city: "Palermo",
+hobby: "Dancing",
+favoriteFood: "Curry",
+favoriteVideoGame: "Death Stranding",
+favoriteFilm: "",
+favoriteBook: "Demian by Hermann Hesse",
+namePet: "Yupi"
+}
+];
+
