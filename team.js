2064280cf3403d627a290faab61a235a8686d6c7
Dennis <emanuele.glorioso@gmail.com>
2023-07-13 16:37:59 +0200

    added dati Dennis

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-let members = [];
+let members = [{
+    name:"Dennis",
+    surname:"Glorioso",
+    age: 20,
+    city:"Licata",
+    hobby:"Videogames",
+    favoriteFood: "pasta",
+    favoriteVideoGame: "God of war",
+    favoriteFilm: "Interstellar",
+    favoriteBook: "Percy Jackson",
+    petName: ""
+}];
