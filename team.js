9106d2233bc59db3e039aad4acc11939b2edf819
Marco <tassonemarco67@gmail.com>
2023-07-13 17:14:25 +0200

    dati Marco

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let members = [];
+let members = [
+    {
+        name: "Marco",
+        surname: "Tassone",
+        age: 22,
+        city: "Statte",
+        hobby: "videogames",
+        favoriteFood: "carbonara",
+        favoriteVideoGame: "MegaMan Star Force",
+        favoriteFilm: " ",
+        favoriteBook: " ",
+        petName: " ",
+    }
+];
